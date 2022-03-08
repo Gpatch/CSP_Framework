3f512e1b9beccfd8a7c93e8eea40810e784077b8
OrlovG <s2053510@ed.ac.uk>
2022-03-08 23:47:53 +0000

    Modification to assignvar.py setters and getters. Added methods to assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,37 @@
+import math
+from assignvar import AssignedVar
+
+
 class Assignment:
-    def __init__(self):
-        pass
+    def __init__(self, assignment):
+        self.assignment = assignment
+
+    def lookup_var(self, var):
+        result = -math.inf
+        for i in self.assignment:
+            if i == var:
+                result = i.get_var_value()
+                break
+        return result
+
+    def is_var_assigned(self, var):
+        result = None
+        for i in self.assignment:
+            if i == var:
+                result = i
+                break
+        return result
+
+    def assign(self, var_name, var_val):
+        exists = False
+        new_var = AssignedVar(var_name, var_val)
+        for i in self.assignment:
+            if i.get_var_name() == new_var.get_var_name():
+                i.set_value(var_val)
+                exists = True
+                break
+        if not exists:
+            self.assignment.append(new_var)
+
+
+
--- assignvar.py
+++ assignvar.py
@@ -1,10 +1,16 @@
 class AssignedVar:
     def __init__(self, var, value):
-        self.var = var
+        self.var_name = var
         self.value = value
 
     def get_var_name(self):
-        return self.var
+        return self.var_name
 
     def get_var_value(self):
         return self.value
+
+    def set_var_name(self, name):
+        self.var_name = name
+
+    def set_var_value(self, value):
+        self.value = value
